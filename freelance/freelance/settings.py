dce0da0ce27b7a7773d6b5a8292a752ee76aa842
redbuzzz <redbuzzzzzz@gmail.com>
2023-05-27 17:35:27 +0300

    added uncompleted project

--- freelance/freelance/settings.py
+++ freelance/freelance/settings.py
@@ -202,7 +202,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'redbuzzzzzz@gmail.com'
-EMAIL_HOST_PASSWORD = 'ilgriukzxtwtwpzq'
+EMAIL_HOST_PASSWORD = 'YOUR_PASSWORD'
 
 CORS_ORIGIN_WHITELIST = [
     "http://localhost:5173",
